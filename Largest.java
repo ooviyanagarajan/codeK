08b6e3fa360ed7a04175925f36f318b0497122cc
ooviyanagarajan <46079755+ooviyanagarajan@users.noreply.github.com>
2019-08-11 12:31:21 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codekata;
+
+/**
+ *
+ * @author crm systems
+ */
+import java.util.*;
+public class Largest {
+    int a,b,c;
+    void check(){
+       System.out.println("Enter three numbers: ");
+       Scanner sc=new Scanner(System.in);
+       a=sc.nextInt();
+       b=sc.nextInt();
+       c=sc.nextInt();
+       if(a>b && a>c){
+           System.out.println(a+" is larger.");
+       }else if(b>a && b>c){
+           System.out.println(b+" is larger.");
+       }else{
+           System.out.println(c+ " is larger.");
+       }
+        
+    }
+    public static void main(String[]args){
+        Largest la=new Largest();
+        la.check();
+        
+    }
+    
+}
