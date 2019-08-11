8ccc60983166d3ba83fcc83ccef291a4419c692b
ooviyanagarajan <46079755+ooviyanagarajan@users.noreply.github.com>
2019-08-11 12:22:00 +0530

    Create Oddeven.java

---
+++ Oddeven.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class Oddeven {
+    int n;
+    void check(){
+        System.out.println("Enter a number: ");
+        Scanner sc=new Scanner(System.in);
+      
+       try{
+           n=sc.nextInt();
+           if(n%2==0){
+            System.out.println("Even number");
+            }else {
+               System.out.println("Odd Number");  
+            }
+           
+       }catch(Exception e) {
+            System.out.println("Invalid input");
+       }
+     
+    }
+    
+   public static void main(String[]args){
+       Oddeven oe=new Oddeven();
+       oe.check();
+   }
+}
