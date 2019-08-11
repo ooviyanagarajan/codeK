f2fdd9208435d153603438308e1d1e65d4bcb6d1
ooviyanagarajan <46079755+ooviyanagarajan@users.noreply.github.com>
2019-08-11 12:34:59 +0530

    Update Arraysum.java

--- Arraysum.java
+++ Arraysum.java
@@ -1,13 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package codekata;
 
-/**
- *
- * @author crm systems
- */
 import java.util.*;
 public class Arraysum {
     void SumofArray(){
