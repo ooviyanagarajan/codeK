8099f5eb3dfe7773bcd4b385efa3dd5fcb7cb971
ooviyanagarajan <46079755+ooviyanagarajan@users.noreply.github.com>
2019-08-11 12:29:28 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,10 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package codekata;
-import java.util.*;
 
+import java.util.*;
 
 public class Alphabet {
     char a;
@@ -23,5 +18,5 @@ public class Alphabet {
        Alphabet al=new Alphabet();
        al.check();
     
-}
+    }
 }
